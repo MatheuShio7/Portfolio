fcca59d26ff4b77f8a26895a541b58ee4d305374
Matheus Shiokawa Silva <matheus.pedro84@hotmail.com>
2024-09-14 23:39:16 -0300

    commit 19

--- script.js
+++ script.js
@@ -111,10 +111,10 @@ window.addEventListener('load', function() {
         this.document.querySelector('.skills-title').innerHTML = 'Habilidades'
 
         this.document.querySelector('.projects-title').innerHTML = 'Projetos'
-        this.document.querySelector('.stock-tracker-div p').innerHTML = 'Stock Tracker é uma plataforma para monitorar seu portfólio de ativos em tempo real. Habilitando o cadastro e obtendo dados importantes como histórico de preços e os últimos 12 ganhos daquele ativo em formato de gráfico. Software disponível em inglês, chinês e português. Todos os dados obtidos através do <a href="https://finance.yahoo.com/" target="_blank">Yahoo Finance</a>.'
-        this.document.querySelector('.seeongit-btn').innerHTML = 'Ver no GitHub'
+        this.document.querySelector('.stockTracker-container p').innerHTML = 'Stock Tracker é uma plataforma para monitorar seu portfólio de ativos em tempo real. Habilitando o cadastro e obtendo dados importantes como histórico de preços e os últimos 12 ganhos daquele ativo em formato de gráfico. Software disponível em inglês, chinês e português. Todos os dados obtidos através do <a href="https://finance.yahoo.com/" target="_blank">Yahoo Finance</a>.'
+        this.document.querySelector('.seeOnGit-btn').innerHTML = 'Ver no GitHub'
 
-        this.document.querySelector('.contact-tittle').innerHTML = 'Contato'
+        this.document.querySelector('.contact-title').innerHTML = 'Contato'
         this.document.querySelector('.btn-cv').innerHTML = 'Baixar CV'
         this.document.querySelector('.btn-cv').href = 'images/CV(pt-br).pdf'
     } else {
